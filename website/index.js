3fadd6919fb918558fd091a6d542b8af7e924c52
Vaneet Vinayak <[vinayaks.64606@gmail.com]>
2024-09-01 18:37:37 -0400

    update index.js

--- website/index.js
+++ website/index.js
@@ -1,8 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch(
-        "https://pojwkd744bohbccwfxw24w5m7y0mffjr.lambda-url.us-east-1.on.aws/"
-    );
+    let response = await fetch("https://pojwkd744bohbccwfxw24w5m7y0mffjr.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
 }
